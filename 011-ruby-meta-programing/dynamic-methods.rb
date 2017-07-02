b692f9a8a08afad5fafd61c51761bf6112945590
afeiship <1290657123@qq.com>
2017-07-02 14:01:46 +0800

    issue: dynamic defin method

---
+++ 011-ruby-meta-programing/dynamic-methods.rb
@@ -0,0 +1,15 @@
+class DynamicMethodsDemo
+    %w(hello hi say).each do |method|
+        define_method method do
+            p 'hello+++> #{$method}'
+        end
+    end
+end
+
+
+
+demo1 = DynamicMethodsDemo.new
+
+demo1.hello
+demo1.hi
+demo1.say
