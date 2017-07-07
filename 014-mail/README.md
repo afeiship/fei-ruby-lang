03d87f83dffa3c18fbc3f60b397e1e60aa4821ec
afeiship <1290657123@qq.com>
2017-07-07 21:21:21 +0800

    issue: add email demo

---
+++ 014-mail/README.md
@@ -0,0 +1,5 @@
+# mail
+> Ruby mailer
+
+## resource:
++ http://cheenwe.cn/2016-10-09/Ruby-Send-Mail/
---
+++ 014-mail/mailer.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/ruby
+
+require 'mail'
+
+smtp_163 = {
+    :address => 'smtp.163.com',
+    :port => 25,
+    :domain => '163.com',
+    :user_name => 'afeidisk000@163.com',
+    :password => 'PASSWORD',
+    :enable_starttls_auto => true,
+    :openssl_verify_mode => 'none'
+}
+
+Mail.defaults { delivery_method :smtp, smtp_163 }
+
+mail = Mail.new do
+  from 'afeidisk000@163.com'
+  to '1290657123@qq.com'
+  subject '这是邮件主题-163[AutoEmail]'
+  body 'body:hello send mail way 2 :)'
+  # add_file '/Users/chenwei/Desktop/spritesheet.png'
+  # add_file :filename => 'spritesheet.png', :content => File.read('/Users/chenwei/Desktop/spritesheet.png')
+end
+
+mail.deliver!
