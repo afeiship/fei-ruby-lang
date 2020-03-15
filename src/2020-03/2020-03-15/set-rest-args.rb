ca16716816a44418661bcd2778019a7954495e14
afeiship <1290657123@qq.com>
2020-03-15 11:40:10 +0800

    add: rest args for ruby - but not work

--- src/2020-03/2020-03-15/set-rest-args.rb
+++ src/2020-03/2020-03-15/set-rest-args.rb
@@ -3,3 +3,10 @@ def gather_arguments(first: nil, **rest)
 end
 
 gather_arguments(first: "only one args", name: "fei", github: "afeiship", hobby: "programing,photography")
+
+hash1 = { name: "fei", github: "afeiship", hobby: "programing,photography" }
+
+# NOT WORK
+name, *rest = hash1
+puts "----------------------------------------------------------------"
+puts rest
